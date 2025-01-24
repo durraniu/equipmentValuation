a88c8d224d32eba94b93002a96255c1f300c0e53
CommodoreBacon <nick@hoovercon.com>
2025-01-24 08:35:33 -0600

    Working dummyfile builder, and first draft of master_list.Rdata

--- DummyFileBuilder.R
+++ DummyFileBuilder.R
@@ -2,37 +2,45 @@
 library(tidyverse)
 
 
-load_old_data <- function(UnitNo, Model, master_list) {
+load_old_data <- function(master_list) {
 
   # Load the file
-  datapath <- file.choose()
-  load(datapath)  # This loads variables into the current environment
+  #datapath <- file.choose()
+  datapath <- choose.dir()
+
+  files <- list.files(datapath)
 
   # Ensure master_list exists
   if (is.null(master_list)) {
     master_list <- list(Equip_List = list(), Market_Hist = list())
   }
 
-  # Create formatted names
-  equip_name <- UnitNo
+  for (i in files) {
 
-  # Define new equipment data
-  unit_data <- list(
-    unit = inpput_values$lot,
-    year = inpput_values$year,
-    hours = inpput_values$hours,
-    description = inpput_values$description,
-    model = inpput_values$model,
-    valuationType = inpput_values$valuationType,
-    condition = inpput_values$condition
-  )
+    load(paste0(datapath,"\\",i))  # This loads variables into the current environment
 
-  # Define new market history data
-  hist_data <- rhandsontable::hot_to_r(inpput_values$HistTable)
+    # Define new equipment data
+    unit_data <- list(
+      unit = inpput_values$lot,
+      year = inpput_values$year,
+      hours = inpput_values$hours,
+      description = inpput_values$description,
+      model = inpput_values$model,
+      valuationType = inpput_values$valuationType,
+      condition = inpput_values$condition,
+      valuation = inpput_values$valuation
+    )
 
-  # Append or update the entries in master_list
-  master_list$Equip_List[[equip_name]] <- unit_data
-  master_list$Market_Hist[[Model]] <- hist_data
+
+    # Define new market history data
+    hist_data <- rhandsontable::hot_to_r(inpput_values$HistTable)
+
+    # Append or update the entries in master_list
+    master_list$Equip_List[[paste0(unit_data$unit)]] <- unit_data
+    master_list$Market_Hist[[paste0(inpput_values$model)]] <- hist_data
+  }
+
+
 
   return(master_list)
 }
@@ -42,9 +50,8 @@ master_list <- NULL
 
 # Call function iteratively
 
-master_list <- load_old_data("14807", "CAT374", master_list)
-master_list <- load_old_data("40709", "GD655", master_list)
+master_list <- load_old_data(master_list)
 
 # Print results
-print(master_list)
-
+master_list$Market_Hist$DX800
+master_list$Market_Hist$`773B`
